f7a2b0f3728f6256ff12f7508a434f67b7dec226
Joseph Duffy <JosephDuffy@me.com>
2015-12-09 23:15:41 +0000

    Updated project to generate a framework
    Updated project iOS deployment target to iOS 8
    Recompiled the static library with bitcode
    Removed example app and tests target

---
+++ XbICalendar/XBICalendar/XbICalendar.h
@@ -0,0 +1,30 @@
+//
+//  XblCalendar.h
+//  XblCalendar
+//
+//  Created by Joseph Duffy on 09/12/2015.
+//  Copyright © 2015 GaltSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XblCalendar.
+FOUNDATION_EXPORT double XblCalendarVersionNumber;
+
+//! Project version string for XblCalendar.
+FOUNDATION_EXPORT const unsigned char XblCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XblCalendar/PublicHeader.h>
+
+#import "ical.h"
+
+#import "XbICComponent.h"
+#import "XbICVEvent.h"
+#import "XbICVCalendar.h"
+#import "XbICInvite.h"
+
+#import "XbICProperty.h"
+#import "XbICPerson.h"
+
+#import "XbICFile.h"
+#import "XbICZoneDirectory.h"
